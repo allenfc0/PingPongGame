86387867af11daf54d41d34d1c3bf1d8b6ac917c
Allen Farias <allenfarias29@gmail.com>
2023-10-04 11:10:24 -0700

    Control what happens in the game

---
+++ src/GamePanel.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel extends JPanel implements Runnable {
+    public GamePanel() {
+
+    }
+
+    public void newBall() {
+
+    }
+
+    public void newPaddles() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkCollision() {
+
+    }
+
+    public void run() {
+
+    }
+
+    public class ActionListener extends KeyAdapter {
+        public void keyPressed(KeyEvent k) {
+
+        }
+
+        public void keyReleased(KeyAdapter k) {
+
+        }
+    }
+}
