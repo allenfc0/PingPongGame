3951df7edb00759cd997c38f30383c56d3907bda
Allen Farias <allenfarias29@gmail.com>
2023-10-05 19:33:44 -0700

    added frame look, settings

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
-    GamePanel panel = new GamePanel();
+    GamePanel panel;
+
     public GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Ping Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
 
     }
 }
