8118884f19f67b545c0aea9e7db43cb3cc7828f9
Allen Farias <allenfarias29@gmail.com>
2023-10-06 21:48:08 -0700

    Defining Paddle class

---
+++ src/Paddle.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Paddle extends Rectangle {
+
+    int id;
+    int yVelocity;
+
+    public Paddle(int x, int y, ) {
+
+    }
+
+    public void keyPressed(KeyEvent k) {
+
+    }
+
+    public void keyReleased(KeyEvent k) {
+
+    }
+
+    public void setYDirection(int yDirection) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+}
