4468d5b2bddb80f6faaa1150e63497c5cde7b69a
Allen Farias <allenfarias29@gmail.com>
2023-10-07 22:57:32 -0700

    Defining Ball class

---
+++ src/Ball.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Ball extends Rectangle {
+
+    Random random;
+    int xVelocity;
+    int yVelocity;
+
+    public Ball() {
+
+    }
+
+    public void setXDirection(int randomXDirection) {
+
+    }
+
+    public void setYDirection(int randomYDirection) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+
+}
