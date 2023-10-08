f31d99571ce9051a2bbd522940d94540780c4154
Allen Farias <allenfarias29@gmail.com>
2023-10-08 14:26:08 -0700

    Defined Paddle class and added KeyEvents for user input

--- src/Paddle.java
+++ src/Paddle.java
@@ -5,28 +5,78 @@ public class Paddle extends Rectangle {
 
     int id;
     int yVelocity;
+    int speed;
 
-    public Paddle(int x, int y, ) {
-
+    public Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
+        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        this.id = id;
+        speed = 10;
     }
 
     public void keyPressed(KeyEvent k) {
-
+        switch(id) {
+            case 1:
+                if(k.getKeyCode() == KeyEvent.VK_W) {
+                    setYDirection(-speed);
+                    move();
+                }
+                if(k.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(speed);
+                    move();
+                }
+                break;
+            case 2:
+                if(k.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(-speed);
+                    move();
+                }
+                if(k.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(speed);
+                    move();
+                }
+                break;
+        }
     }
 
     public void keyReleased(KeyEvent k) {
-
+        switch(id) {
+            case 1:
+                if(k.getKeyCode() == KeyEvent.VK_W) {
+                    setYDirection(0);
+                    move();
+                }
+                if(k.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(0);
+                    move();
+                }
+                break;
+            case 2:
+                if(k.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(0);
+                    move();
+                }
+                if(k.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(0);
+                    move();
+                }
+                break;
+        }
     }
 
     public void setYDirection(int yDirection) {
-
+        yVelocity = yDirection;
     }
 
     public void move() {
-
+        y = y + yVelocity;
     }
 
     public void draw(Graphics g) {
-
+        if(id == 1) {
+            g.setColor(Color.BLUE);
+        } else {
+            g.setColor(Color.RED);
+        }
+        g.fillRect(x, y, width, height);
     }
 }
