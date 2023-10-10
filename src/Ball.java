635f7448d4c4b9ae0c4e3d933c9e2acb46005ce6
Allen Farias <allenfarias29@gmail.com>
2023-10-10 02:24:46 -0700

    Draws and moves a ball when the Ball object is created

--- src/Ball.java
+++ src/Ball.java
@@ -6,25 +6,50 @@ public class Ball extends Rectangle {
     Random random;
     int xVelocity;
     int yVelocity;
+    int initialSpeed;
+    public Ball(int x, int y, int width, int height) {
+        //creating a ball
+        super(x, y, width, height);
 
-    public Ball() {
+        initialSpeed = 2;
+        random = new Random();
 
+        //random roll for x and y
+        //int rollDirection;
+
+        //setting the x direction for where the ball will go
+        int randomXDirection = random.nextInt(2);
+        if(randomXDirection == 0) {
+            randomXDirection--;
+        }
+        setXDirection(randomXDirection * initialSpeed);
+
+        //setting the y direction for where the ball will go
+        int randomYDirection = random.nextInt(2);
+        if(randomYDirection == 0) {
+            randomYDirection--;
+        }
+        setYDirection(randomYDirection * initialSpeed);
     }
 
     public void setXDirection(int randomXDirection) {
-
+        //
+        xVelocity = randomXDirection;
     }
 
     public void setYDirection(int randomYDirection) {
-
+        //
+        yVelocity = randomYDirection;
     }
 
     public void move() {
-
+        x += xVelocity;
+        y += yVelocity;
     }
 
     public void draw(Graphics g) {
-
+        g.setColor(Color.WHITE);
+        g.fillOval(x, y, height, width);
     }
 
 
