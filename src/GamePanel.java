7706544e4217567057b7883be68bc0a26d50aa6a
Allen Farias <allenfarias29@gmail.com>
2023-10-09 23:20:00 -0700

    Added paddles and ball. Checked collision with environment. Added listeners for keyboard keys

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,47 +2,142 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements Runnable {
-    public GamePanel() {
 
+    public static final int GAME_WIDTH = 1000;
+    public static final int GAME_HEIGHT = (int)(GAME_WIDTH * 0.5555);
+    public static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+    public static final int BALL_DIAMETER = 20;
+    public static final int PADDLE_WIDTH = 25;
+    public static final int PADDLE_HEIGHT = 100;
+    Thread gameThread;
+    Image image;
+    Graphics graphics;
+    Random random;
+    Paddle paddle1;
+    Paddle paddle2;
+    Ball ball;
+    Score score;
+
+    public GamePanel() {
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new ActionListener());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     public void newBall() {
-
+        //random = new Random();
+        ball = new Ball((GAME_WIDTH / 2) - (BALL_DIAMETER / 2), (GAME_HEIGHT / 2) - (BALL_DIAMETER / 2), BALL_DIAMETER, BALL_DIAMETER);
     }
 
     public void newPaddles() {
-
+        paddle1 = new Paddle(0, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 1);
+        paddle2 = new Paddle(GAME_WIDTH - PADDLE_WIDTH, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 2);
     }
 
     public void paint(Graphics g) {
-
+        image = createImage(getWidth(), getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image, 0, 0, this);
     }
 
     public void draw(Graphics g) {
-
+        paddle1.draw(g);
+        paddle2.draw(g);
+        ball.draw(g);
     }
 
     public void move() {
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
-
     public void checkCollision() {
+        //check collision for paddles and walls
+        if(paddle1.y <= 0) {
+            paddle1.y = 0;
+        }
+        if(paddle1.y >= GAME_HEIGHT - PADDLE_HEIGHT) {
+            paddle1.y = GAME_HEIGHT - PADDLE_HEIGHT;
+        }
 
+        if(paddle2.y <= 0) {
+            paddle2.y = 0;
+        }
+        if(paddle2.y >= GAME_HEIGHT - PADDLE_HEIGHT) {
+            paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
+        }
+
+        //check collision for ball and walls
+        if(ball.y <= 0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if(ball.y >= GAME_HEIGHT - BALL_DIAMETER) {
+            ball.setYDirection(ball.yVelocity);
+        }
+
+        if(ball.intersects(paddle1)) {
+           ball.xVelocity = Math.abs(ball.xVelocity);
+           ball.xVelocity++;
+           if(ball.yVelocity > 0) {
+               ball.yVelocity++;
+           } else {
+               ball.yVelocity--;
+           }
+           ball.setXDirection(ball.xVelocity);
+           ball.setYDirection(ball.yVelocity);
+        }
+
+        if(ball.intersects(paddle2)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++;
+            if(ball.yVelocity > 0) {
+                ball.yVelocity++;
+            } else {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
     }
 
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1_000_000_000/ amountOfTicks;
+        double delta = 0;
 
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ ns;
+            lastTime = now;
+            if(delta >= 1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
     }
 
     public class ActionListener extends KeyAdapter {
         public void keyPressed(KeyEvent k) {
-
+            paddle1.keyPressed(k);
+            paddle2.keyPressed(k);
         }
 
-        public void keyReleased(KeyAdapter k) {
-
+        public void keyReleased(KeyEvent k) {
+            paddle1.keyReleased(k);
+            paddle2.keyReleased(k);
         }
     }
 }
